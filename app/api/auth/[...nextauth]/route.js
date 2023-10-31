61a4f00db8e2e0d2b3c9419ed3367c29d768f115
hebaarafat159 <heba.arafat159@gmail.com>
2023-10-31 09:19:35 +0000

    fix saving user twice issue

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,12 +2,50 @@ import NextAuth from 'next-auth/next'
 import GoogleProvider from 'next-auth/providers/google'
 
 const handler = NextAuth({
+
+    site: process.env.SITE,
     providers:[
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-        })
-    ]
+            GoogleProvider({
+                clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+                clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+            })
+        ],
+    // database: process.env.MONGODB_URI,
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        signIn: async (user, account, profile) => {
+        // Doesnt appear in my console, by either signing in via Google or Email
+        console.log("the best user,", user);
+        // console.log(`the rest user data ${user.user.name}`);
+        
+        const URL = "http://localhost:3000/api/user"
+            await fetch(URL,{
+                method: "POST",
+                headers:{
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify({
+                            name: user.user.name,
+                            email: user.user.email,
+                            avatar: user.user.image
+                        })
+            })
+            .then(res => 
+            {
+                if(res.status === 200){
+                    //setUserSaved(true)
+                    console.log(`Start Login User : ${JSON.stringify(res)}`);
+                    console.log('Saved Successfully');
+                }
+            }).catch (error => {
+                console.log(`Saving error : ${error}`);
+            })
+        return Promise.resolve(true);
+        },
+    },
+    // session: {
+    //     jwt: true,
+    // }
 })
 
 export {handler as GET, handler as POST}
--- components/UserInfo/UserInfo.jsx
+++ components/UserInfo/UserInfo.jsx
@@ -6,60 +6,55 @@ import { signIn, signOut, useSession } from 'next-auth/react'
 export default function UserInfo() {
 
     const {data: session} = useSession();
-    const [userSaved, setUserSaved] = useState(false);
+    const [userSaved, setUserSaved] = useState(true);
 
-    async function loginUser(user){
-        try{
-            const URL = "http://localhost:3000/api/user"
-            await fetch(URL,{
-                method: "POST",
-                headers:{
-                    "Content-Type" : "application/json"
-                },
-                body: JSON.stringify({
-                            name: user.name,
-                            email: user.email,
-                            avatar: user.image
-                        })
-            })
-            .then(res => 
-            {
-                if(res.status === 200){
-                    setUserSaved(true)
-                    console.log(`Start Login User : ${res.created}`);
-                    console.log('Saved Successfully');
-                }
-            }).catch (error => {
-                console.log(`Saving error : ${error}`);
-            })
-            
-        }catch(error){
-            setUserSaved(false);
-            console.log(`Saving error : ${error}`);
-        }
-    }
+    /**
+     * send user data to server to be saved in mongoose database, 
+     * if the user exists update last login timestape
+     * @param {*} user 
+     */
+    // async function loginUser(user){
+    //         const URL = "http://localhost:3000/api/user"
+    //         await fetch(URL,{
+    //             method: "POST",
+    //             headers:{
+    //                 "Content-Type" : "application/json"
+    //             },
+    //             body: JSON.stringify({
+    //                         name: user.name,
+    //                         email: user.email,
+    //                         avatar: user.image
+    //                     })
+    //         })
+    //         .then(res => 
+    //         {
+    //             if(res.status === 200){
+    //                 setUserSaved(true)
+    //                 console.log(`Start Login User : ${JSON.stringify(res.data)}`);
+    //                 console.log('Saved Successfully');
+    //             }
+    //         }).catch (error => {
+    //             console.log(`Saving error : ${error}`);
+    //         })
+    // }
 
     if(session && session.user){
-        console.log(`Start Login User : ${JSON.stringify(session.user)}`);
-        loginUser(session.user)
-        if(userSaved)
-        {
-            return(
-                <div className="flex gap-4 ml-auto">
-                    <button onClick={() => signOut()} className='text-red-600'>
-                        Logout
-                    </button>
-                    <div className="flex gap-4 ml-auto" onClick={()=>{}}>
-                        <p className="text-sky-600">{ session?.user?.name }</p>
-                        <img 
-                        className="rounded-full" 
-                        src={session?.user?.image} 
-                        width={40} 
-                        height={40}/> 
-                    </div>
+        console.log(`before callback:::::`)
+        return(
+            <div className="flex gap-4 ml-auto">
+            <button onClick={() => signOut()} className='text-red-600'>
+                Logout
+            </button>
+            <div className="flex gap-4 ml-auto" onClick={()=>{}}>
+                <p className="text-sky-600">{ session?.user?.name }</p>
+                <img 
+                    className="rounded-full" 
+                    src={session?.user?.image} 
+                    width={40} 
+                    height={40}/> 
                 </div>
-            )
-        }
+            </div>
+        )
     }
     return(
         <button onClick={() => signIn()} className='text-green-600 ml-auto'>
