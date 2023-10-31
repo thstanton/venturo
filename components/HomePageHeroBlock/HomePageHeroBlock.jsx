903990a4a964a3f397f79a31dcd0b32da1f2a84a
thstanton <thstanton@proton.me>
2023-10-31 17:41:07 +0000

    Home page hero block with text

--- components/HomePageHeroBlock/HomePageHeroBlock.jsx
+++ components/HomePageHeroBlock/HomePageHeroBlock.jsx
@@ -1,11 +1,18 @@
+import { Typography } from '@mui/joy'
+import './HomePageHeroBlock.css'
+
 export default function HomePageHeroBlock() {
 
   return (
-    <div className="video-wrapper">
-        <video playsInline autoPlay muted loop>
+    <div className="HomePageHeroBlock">
+        <video playsInline autoPlay muted loop poster="/waterfall.png">
             <source src="/waterfall_1.mp4" type="video/mp4" />
             Your browser does not support the video tag
         </video>
+        <div className="header">
+          <h1>Venturo</h1>
+          <h2>Find your next adventure</h2>
+        </div>
     </div>
   )
 }
