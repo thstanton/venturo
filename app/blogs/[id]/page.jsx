5bbeed8fdcfd83844eeac360781308e24d78fd29
thstanton <thstanton@proton.me>
2023-10-31 09:34:18 +0000

    Saving file

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -8,16 +8,16 @@ export default function SinglePost({ params }) {
 
   return (
     <div className='SinglePost'>
-      <BlogHeaderBlock />
+      <BlogHeaderBlock image={image} />
       <Grid container spacing={2} sx={{ flexGrow: 1 }}>
         <Grid xs={8}>
-            <BlogTitleBlock />
+            <BlogTitleBlock title={title} intro={intro} />
         </Grid>
         <Grid xs={4}>
-            <BlogLocationWidget />
+            <BlogLocationWidget location={location} />
         </Grid>
         <Grid xs={12}>
-          <BlogImageGallery />
+          <BlogImageGallery images={images} />
         </Grid>
         <Grid xs={8}>
           <p>{ params.id } This will be the body of the post Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia maiores voluptate earum eligendi sit obcaecati, repellat id maxime reiciendis, modi, quisquam praesentium enim? Harum quam debitis quaerat, obcaecati totam nam?</p>
