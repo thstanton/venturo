18e6dd0772175d5469a8bc76a14d65306c3b348e
hebaarafat159 <heba.arafat159@gmail.com>
2023-11-02 19:45:03 +0000

    add collections to user profile

--- app/user/page.jsx
+++ app/user/page.jsx
@@ -1,33 +1,41 @@
 "use client"
+import CollectionList from "@/components/CollectionList/CollectionList";
 import UserBlogs from "@/components/UserBlogs/UserBlogs";
 import UserInfo from "@/components/UserInfo/UserInfo";
 import { checkUserObject } from "@/utilities/utility";
 
-import { Card, Typography, Divider} from "@mui/joy";
-import { useState,useEffect } from "react";
+import { Card, Typography, Divider } from "@mui/joy";
+import { useState, useEffect } from "react";
 
 export default function UserPage() {
     const [user, setUser] = useState(null)
     useEffect(() => {
-        async function loadUser(){
+        async function loadUser() {
             const user = await checkUserObject();
             // console.log(`User Page ::::: ${JSON.stringify(user)}`);
             setUser(user);
         }
-        
+
         loadUser();
-    },[])
-    
-    return(
-        <>        
-        {user ? <Card>
-            <Typography level="title-md">Personal info</Typography>
-            <Divider />
-            <UserInfo user={user}/>
-            <Divider />
-            <UserBlogs user={user}/>
-            </Card> 
-            : null}
+    }, [])
+
+    return (
+        <>
+            {user ? <Card>
+                <Typography level="title-md">Personal info</Typography>
+                <Divider />
+                <UserInfo user={user} />
+                <Divider />
+                <UserBlogs user={user} />
+
+                {(user) ?
+                    <>
+                        < Divider />
+                        <CollectionList />
+                    </> : null
+                }
+            </Card>
+                : null}
         </>
 
     )
